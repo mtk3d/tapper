c9f19b1003d3086185aa58a78c1162ddb7bf88e2
Mateusz Cholewka <mateusz@cholewka.com.pl>
2025-04-09 16:31:05 +0200

    feat(runtime): remove namespace from helpers

--- src/helpers.php
+++ src/helpers.php
@@ -2,14 +2,16 @@
 
 declare(strict_types=1);
 
-namespace Tapper;
-
-function tp($value, string $label = 'debug'): void
-{
-    (new \Tapper\Runtime\Tapper)->tap($value, $label);
+if (! function_exists('tp')) {
+    function tp($value, string $label = 'debug'): void
+    {
+        (new \Tapper\Runtime\Tapper)->tap($value, $label);
+    }
 }
 
-function tpp(string $message = 'Paused'): void
-{
-    (new \Tapper\Runtime\Tapper)->tapPause($message);
+if (! function_exists('tpp')) {
+    function tpp(string $message = 'Paused'): void
+    {
+        (new \Tapper\Runtime\Tapper)->tapPause($message);
+    }
 }
