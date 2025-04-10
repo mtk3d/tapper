a958b3a16b8a7e72f80ca0f5e1f15fbbb56ec940
Mateusz Cholewka <mateusz@cholewka.com.pl>
2025-04-10 21:17:04 +0200

    fix(tui): add global to event

--- src/Console/CommandAttributes/OnEvent.php
+++ src/Console/CommandAttributes/OnEvent.php
@@ -7,6 +7,8 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class OnEvent
 {
+    public bool $global = true;
+
     public function __construct(
         public readonly string $key,
     ) {}
