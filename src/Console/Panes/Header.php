dfa17d052fc0a8f0db0501e74129bc941ab39b23
Mateusz Cholewka <mateusz@cholewka.com.pl>
2025-04-10 21:17:38 +0200

    feat(tui): change LIVE dot to red

--- src/Console/Panes/Header.php
+++ src/Console/Panes/Header.php
@@ -5,7 +5,6 @@ namespace Tapper\Console\Panes;
 use PhpTui\Tui\Display\Area;
 use PhpTui\Tui\Extension\Core\Widget\BlockWidget;
 use PhpTui\Tui\Extension\Core\Widget\ParagraphWidget;
-use PhpTui\Tui\Style\Style;
 use PhpTui\Tui\Text\Span;
 use PhpTui\Tui\Widget\Borders;
 use PhpTui\Tui\Widget\BorderType;
@@ -36,12 +35,11 @@ class Header extends Pane
             BlockWidget::default()
                 ->borders(Borders::BOTTOM)
                 ->borderType(BorderType::Plain)
-                ->borderStyle($this->isActive ? Style::default()->white() : Style::default()->gray())
                 ->widget(
                     ParagraphWidget::fromSpans(
                         Span::fromString(sprintf('Tapper %s | port: %s |', $this->appState->version, $this->appState->port)),
                         Span::fromString(' '),
-                        Span::fromString($live ? ($this->show ? '●' : ' ') : '⏸'),
+                        $live ? Span::fromString($this->show ? '●' : ' ')->red() : Span::fromString('⏸')->blue(),
                         Span::fromString(' '),
                         Span::fromString($live ? 'LIVE' : 'PAUSED'),
                     ),
